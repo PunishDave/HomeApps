2903d7a4f802e3d1a7ce48ef02ae8e79b8d9b790
Dave Berry <insathius@gmail.com>
2025-12-27 14:58:38 +0000

    Import correction

--- app/src/main/java/com/punishdave/homeapps/HaveWeGotScreen.kt
+++ app/src/main/java/com/punishdave/homeapps/HaveWeGotScreen.kt
@@ -15,9 +15,9 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.pointerinput.PointerInputScope
-import androidx.compose.foundation.pointerinput.pointerInput
 import androidx.compose.foundation.gestures.detectVerticalDragGestures
+import androidx.compose.ui.input.pointer.PointerInputScope
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Refresh
