ace0c78e74d9a4a5dc845941d4196e636264c226
Dave Berry <insathius@gmail.com>
2025-12-30 09:53:51 +0000

    Forcing kotlin resolve

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,14 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    resolutionStrategy {
+        // Force Kotlin plugins to resolve from Maven Central at the specified version
+        eachPlugin {
+            if (requested.id.id.startsWith("org.jetbrains.kotlin")) {
+                useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
+            }
+        }
+    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
